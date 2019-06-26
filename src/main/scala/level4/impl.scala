36f83cca131c4ce793b6c0ab310f78b5d7e0f43d
Ikenna Darlington Ogbajie <iogbajie@gmail.com>
2019-06-26 17:08:04 +0200

    Level 4 solutions

--- src/main/scala/level4/impl.scala
+++ src/main/scala/level4/impl.scala
@@ -9,20 +9,20 @@ object impl {
     *
     * Implement me and others!!!
     */
-  def repeat[F[_], O](s: Stream[F, O]): Stream[F, O] = ???
+  def repeat[F[_], O](s: Stream[F, O]): Stream[F, O] = s.repeat
 
   /**
     * Strips all output from a stream
     */
-  def drain[F[_], O](s: Stream[F, O]): Stream[F, INothing] = ???
+  def drain[F[_], O](s: Stream[F, O]): Stream[F, INothing] = s.drain
 
   /**
     * Runs an effect and ignores its output
     */
-  def eval_[F[_], A](fa: F[A]): Stream[F, INothing] = ???
+  def eval_[F[_], A](fa: F[A]): Stream[F, INothing] = Stream.eval_(fa)
 
   /**
     * Catches any errors produced by a stream
     */
-  def attempt[F[_], O](s: Stream[F, O]): Stream[F, Either[Throwable, O]] = ???
+  def attempt[F[_], O](s: Stream[F, O]): Stream[F, Either[Throwable, O]] = s.attempt
 }
